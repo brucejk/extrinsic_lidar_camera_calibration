8b48ae366d88dbe0b28d1803bb1b27b0aca6d2af
brucejk <bjhuang@umich.edu>
2019-10-02 21:03:44 -0400

    works for one scan

--- processLidarData.m
+++ processLidarData.m
@@ -1,23 +1,24 @@
 clear; clc;
 
 % Single variable called point_cloud
-mat_file_path = 'LiDARTag_data/velodyne_points-verification3--2019-09-03-23-03.mat';
-pc = loadPointCloud(mat_file_path, bagfile);
-data = getPayload(pc, : , :);
-[scans, points_per_scan, values] = size(data.point_cloud);
+mat_file_path = '../repo/LiDARTag_data/velodyne_points-lab8-closer-big--2019-09-06-15-28.mat';
+pc = loadPointCloud(mat_file_path);
+scans = 1;
+data = getPayload(pc, 1 , 1); % XYZIR 
+[values, points_per_scan] = size(data);
 
 %% Step 1: Calculate the d, theta, phi from the x, y, z of point  cloud
 % d     - length of beam
 % theta - azimuth angle off of x axis
 % phi   - elevation angle
-xs = data.point_cloud(:,:,1);
-ys = data.point_cloud(:,:,2);
-zs = data.point_cloud(:,:,3);
-ring = data.point_cloud(:,:,5);
+xs = data(1,:);
+ys = data(2,:);
+zs = data(3,:);
+ring = data(5,:);
 
 d     = sqrt(xs.^2 + ys.^2 + zs.^2);
-phi   = atan2(zs ./ sqrt(xs.^2 + ys.^2));
-theta = atan2(ys ./ xs);
+phi   = atan2(zs , sqrt(xs.^2 + ys.^2));
+theta = atan2(ys , xs);
 
 %% Step 2: Calculate 'ground truth' points by projecting the angle onto the
 % normal plane
@@ -26,22 +27,26 @@ theta = atan2(ys ./ xs);
 % plane_normal = [nx ny nz]
 
 % example normal lies directly on the x axis
-plane_normal = [5, 0, 0]; % arbitrary at the moment
+opt.rpy_init = [45 2 3];
+opt.T_init = [2, 0, 0];
+opt.H_init = eye(4);
+opt.method = "Constraint Customize"; %% will add more later on
+opt.UseCentroid = 1;
 
-magnitude_plane_normal = norm(plane_normal, 2);
+[plane_normal, magnitude_plane_normal] = estimateNormal(opt, data(1:3, :), 0.8051);
 
-angles = zeros(scans, points_per_scan, 3);
-angles(:,:,1) = xs ./ d;
-angles(:,:,2) = ys ./ d;
-angles(:,:,3) = zs ./ d;
+angles = zeros(points_per_scan, 3);
+angles(:,1) = xs ./ d;
+angles(:,2) = ys ./ d;
+angles(:,3) = zs ./ d;
 
-normals = zeros(scans, points_per_scan, 3);
-normals(:,:,1) = plane_normal(1)*ones(scans, points_per_scan);
-normals(:,:,2) = plane_normal(2)*ones(scans, points_per_scan);
-normals(:,:,3) = plane_normal(3)*ones(scans, points_per_scan);
+normals = zeros(points_per_scan, 3);
+normals(:,1) = plane_normal(1)*ones(scans, points_per_scan);
+normals(:,2) = plane_normal(2)*ones(scans, points_per_scan);
+normals(:,3) = plane_normal(3)*ones(scans, points_per_scan);
 
 numerator = magnitude_plane_normal.^2;
-denominator = dot(normals, angles, 3);
+denominator = dot(normals', angles', 1);
 d_hat = numerator ./ denominator;
 
 % d_hat now holds the projection of a point P onto the plane normal
@@ -50,24 +55,29 @@ d_hat = numerator ./ denominator;
 % This delta_d represents the range offset in the LiDAR
 
 num_beams = 32; % config of lidar
-delta_D = zeros(num_beams);
+delta_D = zeros(num_beams, 1);
 
 for n = 1:num_beams
     sorted_d = [];
     sorted_d_hat = [];
-
-    for i=1:scans
-        for j = 1:points_per_scan
-                if( ring(i,j)== n)
-                    sorted_d = [sorted_d, d(i,j)];
-                    sorted_d_hat = [sorted_d_hat, d_hat(i,j)];
-                end
-         end
-    end    
-    delta_D(n)= optimizeDistance(sorted_d, sorted_d_hat);
+    if ~any(ring==n)
+        continue
+    end
+    delta_D(n) = optimizeDistance(d(ring==n), d_hat(ring == n));
+%     for i=1:scans
+%         for j = 1:points_per_scan
+%             if(ring(j)== n)
+%                 sorted_d = [sorted_d, d(i,j)];
+%                 sorted_d_hat = [sorted_d_hat, d_hat(i,j)];
+%             end
+%         end
+%     end    
+%     delta_D(n)= c(sorted_d, sorted_d_hat);
 end
-
-
+disp('done')
+disp(delta_D)
+std(delta_D(delta_D~=0))
+plot(delta_D)
 
 
 
