2bbaf170c4ab1beaa8950b166d2366a226d2bf78
brucejk <bjhuang@umich.edu>
2019-10-02 21:01:24 -0400

    fixed dimension bug

--- optimizeDistance.m
+++ optimizeDistance.m
@@ -1,3 +1,3 @@
 function delta_D = optimizeDistance(sorted_d, sorted_d_hat)
-    delta_D = sum(sorted_d - sorted_d_hat)/size(sorted_d);
+    delta_D = sum(sorted_d - sorted_d_hat)/size(sorted_d,2);
 end
