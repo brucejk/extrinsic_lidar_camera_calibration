007ce77fc44a4fa31d401f8868fa0c6f1a959705
brucejk <bjhuang@umich.edu>
2019-10-02 21:00:47 -0400

    fixed to take ring number

--- getPayload.m
+++ getPayload.m
@@ -1,8 +1,7 @@
 function accumulated_payload = getPayload(point_cloud, pc_iter, num_scan)
-
     accumulated_payload = [];
     for i = 0:num_scan-1
-        points = point_cloud(pc_iter+i, :, 1:3);
+        points = point_cloud(pc_iter+i, :, :);
         points = (reshape(points, size(points, 2),[]))';
         for i = 1:size(points, 2)
             if ~any(all(points(:, i), 2))
@@ -12,5 +11,5 @@ function accumulated_payload = getPayload(point_cloud, pc_iter, num_scan)
         end
         accumulated_payload = [accumulated_payload points];
     end
-    accumulated_payload = [accumulated_payload; ones(1, size(accumulated_payload, 2))];
+%     accumulated_payload = [accumulated_payload; ones(1, size(accumulated_payload, 2))];
 end
