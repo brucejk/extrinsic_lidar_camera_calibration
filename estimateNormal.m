742f702b378a7b9a494d288f9f6826fc6b547655
brucejk <bjhuang@umich.edu>
2019-11-14 23:06:57 -0500

    add normalized normal vectors

--- estimateNormal.m
+++ estimateNormal.m
@@ -1,4 +1,4 @@
-function [normals, distance]= estimateNormal(opt, X, target_len)
+function [plane, distance]= estimateNormal(opt, X, target_len)
     opt = optimizeCost(opt, X, target_len);
     target_lidar = [0 -target_len/2 -target_len/2 1;
                     0 -target_len/2  target_len/2 1;
@@ -6,12 +6,14 @@ function [normals, distance]= estimateNormal(opt, X, target_len)
                     0  target_len/2 -target_len/2 1]';
     corners = inv(opt.H_opt) * target_lidar;
     corners = sortrows(corners', 3, 'descend')';
+    plane.centriod = mean(corners, 2);
     normals = cross(corners(1:3,1)-corners(1:3,2), corners(1:3,1)-corners(1:3,3));
     normals = normals/(norm(normals));
+    plane.unit_normals = normals;
     distance = dot(normals, corners(1:3,1));
-    normals = distance * normals;
+    plane.normals = distance * normals;
     
     if normals(1) < 0
-        normals = -normals;
+        plane.normals = -normals;
     end
 end
