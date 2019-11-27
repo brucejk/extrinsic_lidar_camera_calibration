afbd684560cc74642d7b6d5ee5e45c3a14e8bb48
brucejk <bjhuang@umich.edu>
2019-11-27 11:27:57 -0500

    check if skipping the ring or not

---
+++ checkRingsCrossDataset.m
@@ -0,0 +1,16 @@
+% If not enough data for a ring, don't try to calibrate it
+function skip = checkRingsCrossDataset(data, delta, num_targets, ring)
+    valid_target_num = num_targets;
+    for target = 1:num_targets
+        if size(data{target}(ring).points, 2) == 0
+            delta(ring).H = eye(4);
+            valid_target_num = valid_target_num -1;
+        end
+    end
+
+    if valid_target_num < min(3, num_targets) % less than this, skip the ring
+        skip = true;
+    else
+        skip = false;
+    end
+end
