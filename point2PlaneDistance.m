9d8f8f225b77910e687a038ddd6fcdc7753f7c27
brucejk <bjhuang@umich.edu>
2020-02-03 21:32:53 -0500

    add some statistic outputs

--- point2PlaneDistance.m
+++ point2PlaneDistance.m
@@ -1,20 +1,36 @@
 function distance = point2PlaneDistance(data, plane, num_beams, num_targets)
-    distance(num_beams) = struct();
+    distance.ring(num_beams) = struct();
     for n = 1:num_beams
         X = [];
+        points_array = [];
+        sum_points = 0;
         for t = 1:num_targets
             point = data{t}(n).points;
-            if isempty(point) 
-                continue;
+            num_points = size(point, 2); 
+            if num_points > 0
+                plane_centroids = repmat(plane{t}.centriod, [1, num_points]);
+                points_array = [points_array point];
+                diff = [point - plane_centroids];
+                normals = repmat(plane{t}.unit_normals, [1, num_points]);
+                X = [X abs(dot(normals, diff(1:3,:)))];
             end
-            plane_centroids = repmat(plane{t}.centriod, [1,size(point, 2)]);
-            diff = [point - plane_centroids];
-            normals = repmat(plane{t}.unit_normals, [1,size(point, 2)]);
-            X = [X abs(dot(normals, diff(1:3,:)))];
+            sum_points = sum_points + num_points;
         end
-        distance(n).mean = mean(X,2);
-        distance(n).std = std(X);
+        distance.ring(n).ring = n;
+        distance.ring(n).num_points = sum_points;
+        if sum_points > 0
+            distance.ring(n).mean = mean(X,2);
+            ring_centroid = mean(points_array, 2);
+            distance.ring(n).ring_centroid = ring_centroid(1:3);
+            distance.ring(n).z_axis = ring_centroid(3);
+            distance.ring(n).std = std(X);
+        else
+            distance.ring(n).ring_centroid = [0, 0, 0];
+            distance.ring(n).z_axis = 0;
+            distance.ring(n).mean = 0;
+            distance.ring(n).std = 0;
+        end
+        distance.ring(n).mean_in_mm = distance.ring(n).mean * 1e3;
+        distance.ring(n).std_in_mm = distance.ring(n).std * 1e3;
     end
-
-
 end
