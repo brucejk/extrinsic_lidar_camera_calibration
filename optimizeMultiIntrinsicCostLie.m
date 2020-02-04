f489ba4f0cf7a1629f2ce8e3da789107677690cb
brucejk <bjhuang@umich.edu>
2020-02-04 16:29:50 -0500

    add up costs only for valid targets

--- optimizeMultiIntrinsicCostLie.m
+++ optimizeMultiIntrinsicCostLie.m
@@ -1,6 +1,8 @@
-function cost = optimizeMultiIntrinsicCostLie (X, plane, ring, theta_x, theta_y, theta_z, T, S)
+function cost = optimizeMultiIntrinsicCostLie (X, valid_targets, plane, ring, theta_x, theta_y, theta_z, T, S)
     cost = 0;
     for i = 1: length(X)
-        cost = cost + optimizeIntrinsicCostLie(X{i}(ring), plane{i}, theta_x, theta_y, theta_z, T, S);
+        if valid_targets.valid(1, i)
+            cost = cost + optimizeIntrinsicCostLie(X{i}(ring), plane{i}, theta_x, theta_y, theta_z, T, S);
+        end
     end
 end
--- optimizeMultiIntrinsicCostSpherical.m
+++ optimizeMultiIntrinsicCostSpherical.m
@@ -1,7 +1,9 @@
-function cost = optimizeMultiIntrinsicCostSpherical (X, plane, ring, D_corr, theta_corr, phi_corr)
+function cost = optimizeMultiIntrinsicCostSpherical (X, valid_targets, plane, ring, D_corr, theta_corr, phi_corr)
     cost = 0;
     num_targets = length(X); 
     for i = 1: num_targets
-        cost = cost + optimizeIntrinsicCostSpherical(X{i}(ring), plane{i}, D_corr, theta_corr, phi_corr);
+        if valid_targets.valid(1, i)
+            cost = cost + optimizeIntrinsicCostSpherical(X{i}(ring), plane{i}, D_corr, theta_corr, phi_corr);
+        end
     end
 end
