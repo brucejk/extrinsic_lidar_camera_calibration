8d076fe6f53c53f955901e555f2fe2d0c216253b
brucejk <bjhuang@umich.edu>
2020-02-12 17:42:01 -0500

    allow user change default values

--- checkRingsCrossDataset.m
+++ checkRingsCrossDataset.m
@@ -1,13 +1,18 @@
-function valid_targets = checkRingsCrossDataset(data, num_targets, ring)
+function valid_targets = checkRingsCrossDataset(data, num_targets, ring, minimun_target_for_this_ring, minimun_point_on_a_target)
     % If not 'enough' data for a ring, don't try to calibrate it
     %
-    % For enough data, we mean:
+    % For enough data, by default we mean :
     % (1) on a target, a ring has to have a least 10 points to calibrate
     % (2) on the whole scene (containing many many targets), the ring has
     % to appear on targets at least 5 times.
     
-    minimun_point_on_a_target = 10; 
-    minimun_target_for_this_ring = 5;
+    if ~exist('minimun_point_on_a_target', 'var') 
+        minimun_point_on_a_target = 10;
+    end
+    
+    if ~exist('minimun_target_for_this_ring', 'var') 
+        minimun_target_for_this_ring = 1;
+    end
     
     valid_targets.skip = false;
     valid_targets.valid = zeros(1, num_targets);
@@ -25,6 +30,6 @@ function valid_targets = checkRingsCrossDataset(data, num_targets, ring)
 %     if valid_target_num < min(3, num_targets) % less than this, skip the ring
     if sum(valid_targets.valid) < minimun_target_for_this_ring
         valid_targets.skip = true;
-        % warning..
+        warning("Ring #%i has been skipped due to not enough valid targets %i:", ring, sum(valid_targets.valid));
     end
 end
