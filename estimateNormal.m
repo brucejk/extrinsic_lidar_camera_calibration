4c607ce27dbb1a9b0d4ead5a772f59471955f59f
brucejk <bjhuang@umich.edu>
2020-02-12 17:45:11 -0500

    fixed spelling of variable

--- estimateNormal.m
+++ estimateNormal.m
@@ -6,7 +6,7 @@ function [plane, distance]= estimateNormal(opt, X, target_len)
                     0  target_len/2 -target_len/2 1]';
     corners = inv(opt.H_opt) * target_lidar;
     corners = sortrows(corners', 3, 'descend')';
-    plane.centriod = mean(corners, 2);
+    plane.centroid = mean(corners, 2);
     normals = cross(corners(1:3,1)-corners(1:3,2), corners(1:3,1)-corners(1:3,3));
     normals = normals/(norm(normals));
     plane.unit_normals = normals;
