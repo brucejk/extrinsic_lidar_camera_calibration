c8fca3c7357b3923bc5e26e5076356163e101f70
brucejk <bjhuang@umich.edu>
2020-02-12 17:44:41 -0500

    allow different type of input and skip if one target has no points

--- plotCalibratedResults.m
+++ plotCalibratedResults.m
@@ -1,4 +1,13 @@
 function plotCalibratedResults(num_targets, plane, data_split_with_ring, data)
+    
+    if isfield(data, 'payload_points')
+        % do nothing 
+    elseif ~isfield(data, 'payload_points') && isfield(data, 'points_mat')
+        [data.('payload_points')] = data.('points_mat');
+    else
+        error("input data has not field of payload_points nor points_mat, please check again")
+    end
+
     image_handle = figure('Name', "Calibrated Results", 'Visible', 'off');
     image_handle = axes('parent', image_handle);
     quiver3(image_handle, zeros(3,1),zeros(3,1),zeros(3,1),[1;0;0],[0;1;0],[0;0;1], '-b', 'fill', 'LineWidth', 5)
@@ -10,11 +19,13 @@ function plotCalibratedResults(num_targets, plane, data_split_with_ring, data)
     
     for i =1:num_targets
         w = null(plane{i}.unit_normals'); % Find two orthonormal vectors which are orthogonal to v
-        X = plane{i}.centriod(1)+w(1,1)*P+w(1,2)*Q; % Compute the corresponding cartesian coordinates
-        Y = plane{i}.centriod(2)+w(2,1)*P+w(2,2)*Q; %   using the two vectors in w
-        Z = plane{i}.centriod(3)+w(3,1)*P+w(3,2)*Q;
+        X = plane{i}.centroid(1)+w(1,1)*P+w(1,2)*Q; % Compute the corresponding cartesian coordinates
+        Y = plane{i}.centroid(2)+w(2,1)*P+w(2,2)*Q; %   using the two vectors in w
+        Z = plane{i}.centroid(3)+w(3,1)*P+w(3,2)*Q;
         surf(image_handle, X, Y, Z);
-        scatter3(image_handle, data(i).payload_points(1,:), data(i).payload_points(2,:), data(i).payload_points(3,:), 30, 'b.');
+        if ~isempty(data(i).payload_points)
+            scatter3(image_handle, data(i).payload_points(1,:), data(i).payload_points(2,:), data(i).payload_points(3,:), 30, 'b.');
+        end
         for ring =1:32
             if (isempty(data_split_with_ring{i}(ring).points))
                 continue;
