90b57ee938508528efa1765c6182ece26887f841
brucejk <bjhuang@umich.edu>
2020-02-14 11:54:41 -0500

    formatting

--- ransacLine.m
+++ ransacLine.m
@@ -37,10 +37,11 @@ function [x, y, modelInliers] = ransacLine(points, threshold)
       @(model, points) sqrt(sum((points(:, 2) - polyval(model, points(:,1))).^2,2))/length(points(:,2));
 
     [~, inlierIdx] = ransac(points,fitLineFcn,evalLineFcn, ...
-      sampleSize,maxDistance);
+                            sampleSize,maxDistance);
     modelInliers = polyfit(points(inlierIdx,1), points(inlierIdx,2), 1);
-%             scatter(points(inlierIdx,1), points(inlierIdx,2), 'bo')
+%     scatter(points(inlierIdx,1), points(inlierIdx,2), 'ko')
     inlierPts = points(inlierIdx,:);
+%     scatter(points(inlierIdx,1), points(inlierIdx,2), 'ko')
     x = [min(inlierPts(:,1)) max(inlierPts(:,1))];
     y = modelInliers(1)*x + modelInliers(2);
 end
