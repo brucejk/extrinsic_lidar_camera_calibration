9cbd364b1f4a726fe34ba6443e01f6974e746db6
brucejk <bjhuang@umich.edu>
2020-02-15 14:00:19 -0500

    formatting

--- loadBagImg.m
+++ loadBagImg.m
@@ -30,12 +30,6 @@
 %}
 
 function loadBagImg(img_handle, bag_file_path, bag_file, display, clean)
-    if strcmpi("display", display)
-        display = 1;
-    else
-        display = 0;
-    end
-
     if nargin < 4
         clean=0;
     else
@@ -45,20 +39,28 @@ function loadBagImg(img_handle, bag_file_path, bag_file, display, clean)
     if clean && isvalid(img_handle)
         img_handle.cla;
     end
+    
     % load image
     hold(img_handle, 'on');
-    bagselect = rosbag(bag_file_path + bag_file);
-    bagselect2 = select(bagselect,'Time',...
-        [bagselect.StartTime bagselect.StartTime + 1],'Topic','/camera/color/image_raw');
-    allMsgs = readMessages(bagselect2);
-    [img,~] = readImage(allMsgs{1});
-    imshow(img, 'Parent', img_handle);
-    img_handle.XLim = [0 size(img,2)];
-    img_handle.YLim = [0 size(img,1)];
-    title(img_handle, bag_file)
-    hold(img_handle, 'off');
     
-    if display
+    if ~isempty(bag_file_path)
+        bagselect = rosbag(bag_file_path + bag_file);
+        bagselect2 = select(bagselect,'Time',...
+            [bagselect.StartTime bagselect.StartTime + 1],'Topic','/camera/color/image_raw');
+        allMsgs = readMessages(bagselect2);
+        [img,~] = readImage(allMsgs{1});
+        imshow(img, 'Parent', img_handle);
+        img_handle.XLim = [0 size(img,2)];
+        img_handle.YLim = [0 size(img,1)];
+        title(img_handle, bag_file)
+        hold(img_handle, 'off');
+    else
+        imshow(bag_file.image, 'Parent', img_handle);
+        img_handle.XLim = [0 bag_file.width];
+        img_handle.YLim = [0 bag_file.height];
+    end
+    
+    if checkDisplay(display)
         set(get(img_handle,'parent'),'visible','on');% show the current axes
     end
 end
