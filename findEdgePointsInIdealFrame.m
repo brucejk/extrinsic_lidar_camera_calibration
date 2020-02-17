8476df66a4ab03026246cd24e170413af770af1c
Bruce JK Huang <bjhuang@umich.edu>
2020-02-17 18:27:26 -0500

    one point per scan per ring

--- findEdgePointsInIdealFrame.m
+++ findEdgePointsInIdealFrame.m
@@ -24,45 +24,44 @@ function edge = findEdgePointsInIdealFrame(H_TL, X_ref, tag_length)
     edge(4).points = [];
     
     % choose one point per scan per ring
-%     for j = 1:num_scan
-%         current_scan_points = X_ref(:, X_ref(6,:)==j);
-%         for i = bottom_ring:top_ring
-%             if size(current_scan_points(2:3, current_scan_points(5,:)==i), 2) > 1
-%                 current_ring_points = current_scan_points(1:3, current_scan_points(5,:)==i);
-%                 extreme_points = [current_ring_points(:, 1), current_ring_points(:, end)];
-%                 extreme_point1 = current_ring_points(:, 1);
-%                 extreme_point2 = current_ring_points(:, end);
-%                 dist1 = zeros(1, 4);
-%                 dist2 = zeros(1, 4);
-%                 for k = 1:4
-%                     dist1(1, k) = pointToLineDistance(extreme_point1(2:3, :)', edge_list(k).line(1, :), edge_list(k).line(2, :));
-%                     dist2(1, k) = pointToLineDistance(extreme_point2(2:3, :)', edge_list(k).line(1, :), edge_list(k).line(2, :));
-%                 end
-%                 [~, edge1_i] = min(dist1);
-%                 [~, edge2_i] = min(dist2);
-%                 edge_point_3D_1 = inv(H_TL) * [extreme_point1; 1];
-%                 edge_point_3D_2 = inv(H_TL) * [extreme_point2; 1];
-% %                 selected_edge_points = [current_ring_points(1:3, indices(selected_indices)); ones(1, length(selected_indices))];
-%                 edge(edge1_i).points = [edge(edge1_i).points, edge_point_3D_1];
-%                 edge(edge2_i).points = [edge(edge2_i).points, edge_point_3D_2];
-%             end
-%         end
-%     end
+    for j = 1:num_scan
+        current_scan_points = X_ref(:, X_ref(6,:)==j);
+        for i = bottom_ring:top_ring
+            if size(current_scan_points(2:3, current_scan_points(5,:)==i), 2) > 1
+                current_ring_points = current_scan_points(1:3, current_scan_points(5,:)==i);
+                extreme_point1 = current_ring_points(:, 1);
+                extreme_point2 = current_ring_points(:, end);
+                dist1 = zeros(1, 4);
+                dist2 = zeros(1, 4);
+                for k = 1:4
+                    dist1(1, k) = pointToLineDistance(extreme_point1(2:3, :)', edge_list(k).line(1, :), edge_list(k).line(2, :));
+                    dist2(1, k) = pointToLineDistance(extreme_point2(2:3, :)', edge_list(k).line(1, :), edge_list(k).line(2, :));
+                end
+                [~, edge1_i] = min(dist1);
+                [~, edge2_i] = min(dist2);
+                edge_point_3D_1 = inv(H_TL) * [extreme_point1; 1];
+                edge_point_3D_2 = inv(H_TL) * [extreme_point2; 1];
+%                 selected_edge_points = [current_ring_points(1:3, indices(selected_indices)); ones(1, length(selected_indices))];
+                edge(edge1_i).points = [edge(edge1_i).points, edge_point_3D_1];
+                edge(edge2_i).points = [edge(edge2_i).points, edge_point_3D_2];
+            end
+        end
+    end
     
     
      % choose a few points using all scans
-    for i = bottom_ring:top_ring
-        current_ring_points = X_ref(:, X_ref(5,:)==i);
-%         dist = zeros(length(current_ring_points), 4);
-        for k = 1:4
-            dist = pointToLineDistance(current_ring_points(2:3,:)', edge_list(k).line(1, :), edge_list(k).line(2, :));
-            [value, indices] = mink(dist, pick_points);
-            selected_indices = find (value<tag_length/8);
-            selected_edge_points = inv(H_TL) * [current_ring_points(1:3, indices(selected_indices)); ones(1, length(selected_indices))];
-%             selected_edge_points = [current_ring_points(1:3, indices(selected_indices)); ones(1, length(selected_indices))];
-            edge(k).points = [edge(k).points, selected_edge_points];
-        end
-    end
+%     for i = bottom_ring:top_ring
+%         current_ring_points = X_ref(:, X_ref(5,:)==i);
+% %         dist = zeros(length(current_ring_points), 4);
+%         for k = 1:4
+%             dist = pointToLineDistance(current_ring_points(2:3,:)', edge_list(k).line(1, :), edge_list(k).line(2, :));
+%             [value, indices] = mink(dist, pick_points);
+%             selected_indices = find (value<tag_length/8);
+%             selected_edge_points = inv(H_TL) * [current_ring_points(1:3, indices(selected_indices)); ones(1, length(selected_indices))];
+% %             selected_edge_points = [current_ring_points(1:3, indices(selected_indices)); ones(1, length(selected_indices))];
+%             edge(k).points = [edge(k).points, selected_edge_points];
+%         end
+%     end
     
     
 %     edge_list = [tag_length/2, -tag_length/2, -tag_length/2, tag_length/2];
