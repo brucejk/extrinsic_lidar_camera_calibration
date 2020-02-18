f120ffa794eebf928e374b2af659ecc6425540db
Bruce JK Huang <bjhuang@umich.edu>
2020-02-18 16:39:00 -0500

    return area (still only works for 2D)

--- makeConvexHull.m
+++ makeConvexHull.m
@@ -1,4 +1,4 @@
-function convex_hull = makeConvexHull(X)
+function [convex_hull, area_2D] = makeConvexHull(X)
     X = makeTallMatrix(X);
     if size(X, 2) == 3
         if mean(X(:, 3), 3) == 1
@@ -7,7 +7,7 @@ function convex_hull = makeConvexHull(X)
     else
          X_no_ones = X;
     end
-    index = convhull(X_no_ones);
-    X(:,1:2) = X_no_ones(index(1:end-1), :);
-    convex_hull = X';
+    [index, area_2D] = convhull(X_no_ones);
+    convex_hull = X_no_ones(index(1:end-1), :)';
+%     convex_hull = X';
 end
