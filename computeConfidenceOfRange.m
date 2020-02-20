62cdedbee1c7c7753b0b9093d8e0898842a5e2d4
brucejk <bjhuang@umich.edu>
2020-02-19 22:23:58 -0500

    compute CoR and return a structure for analizing and plotting

--- computeConfidenceOfRange.m
+++ computeConfidenceOfRange.m
@@ -1,12 +1,14 @@
-function confidence_of_range = computeConfidenceOfRange(P, bag_data, indices, method, refinement, fig_title)
-    num_senes = length(indices);
-    title_txt = fig_title;
+function confidence_of_range = computeConfidenceOfRange(P, bag_data, validation_indices, method, refinement)
+    num_senes = length(validation_indices);
+    
     for scene = 1:num_senes % which dataset
-        current_index = indices(scene);
+        current_index = validation_indices(scene);
         num_scan = length(bag_data(current_index).scans(:));
-
+        confidence_of_range(scene).bagfile = bag_data(current_index).bagfile;
+        
         for scan_num = 1:num_scan
             num_tag = size(bag_data(current_index).scans(scan_num).lidar_target, 2);
+            
             for tag_num = 1:num_tag % which tag in this dataset
                 % refinement and no-refinement should be the same
                 if isempty(bag_data(current_index).scans(scan_num).lidar_target(tag_num).(method).corners) 
@@ -14,18 +16,22 @@ function confidence_of_range = computeConfidenceOfRange(P, bag_data, indices, me
                 else
                     if strcmpi(refinement, 'no_refinement')
                         current_corners_X = [bag_data(current_index).scans(scan_num).lidar_target(tag_num).(method).corners];
+                        H_LT = bag_data(current_index).scans(scan_num).lidar_target(tag_num).(method).H_LT;
+                        
                     else
                         current_corners_X = [bag_data(current_index).scans(scan_num).lidar_target(tag_num).(method).refined_corners];
+                        H_LT = bag_data(current_index).scans(scan_num).lidar_target(tag_num).(method).refined_H_LC;
                     end
-                    current_corners_Y = [bag_data(current_index).scans(scan_num).camera_target(tag_num).(method).corners];
-                    scan_cost = verifyCornerAccuracy(current_corners_X(:, 1:4), current_corners_Y(:, 1:4), P);
+                    num_corners = size(H_LT, 2) / 4;
                     
-                    [centroid, normals] = computeCentroidAndNormals(current_corners_X);
-                    dist_to_lidar = norm(centroid);
+                    current_corners_Y = [bag_data(current_index).scans(scan_num).camera_target(tag_num).(method).corners];
+                    squared_sum = verifyCornerAccuracy(current_corners_X(:, 1:4), current_corners_Y(:, 1:4), P);
+                    
+                    confidence_of_range(scene).scans(scan_num).targets(tag_num).RMSE = sqrt(squared_sum / num_corners);
+                    confidence_of_range(scene).scans(scan_num).targets(tag_num).distance  = norm(H_LT(1:3, 4));
+                    confidence_of_range(scene).scans(scan_num).targets(tag_num).rpy = rad2deg(rotm2eul(H_LT(1:3, 1:3), 'xyz'));
                 end
             end
-            title_txt = title_txt + " - ccene: " + bag_data(current_index).bagfile;
         end
     end
-    title(title_txt)
 end
