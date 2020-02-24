7536aee4a28ddea1ec19009ffe2c84a3c9941659
FENGChenxi0823 <cfeng@connect.ust.hk>
2020-02-24 17:05:21 -0500

    trivial

--- optimizeMultiIntrinsicCostBL2.m
+++ optimizeMultiIntrinsicCostBL2.m
@@ -3,7 +3,7 @@ function cost = optimizeMultiIntrinsicCostBL2 (X, valid_targets, plane, ring, D_
     num_targets = length(X); 
     for i = 1: num_targets
         if valid_targets.valid(1, i)
-            cost = cost + optimizeIntrinsicCostBL2(X{i}(ring), plane{i}, D_s, D, A_c, S_vc, C_vc, H_oc, S_voc, C_voc);
+            cost = cost + optimizeIntrinsicCostBL2(X{i}(ring), plane{i}, D_s, D, A_c, S_vc, C_vc, H_oc, S_voc,C_voc);
         end
     end
 end
