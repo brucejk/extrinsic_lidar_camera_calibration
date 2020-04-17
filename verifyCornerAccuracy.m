047a114129151e081f62072629b6944e4f3e5e4b
brucejk <bjhuang@umich.edu>
2020-04-16 23:10:09 -0400

    not use projectPointsToImagePlane function

--- verifyCornerAccuracy.m
+++ verifyCornerAccuracy.m
@@ -32,5 +32,6 @@
 function cost = verifyCornerAccuracy(X_verification, Y_verification, P)
     C_X_transformed = P * X_verification;
     C_X_transformed = C_X_transformed ./ C_X_transformed(3,:);
+%     C_X_transformed = projectPointsToImagePlane(X_verification, P);
     cost = (norm(C_X_transformed(1:2,:) - Y_verification(1:2,:), 'fro'))^2;
 end
