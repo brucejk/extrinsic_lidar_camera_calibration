f15ccdcb6aa2f999c5629c8c8c7b551b71609b98
Brucebot <bjhuang@umich.edu>
2020-08-09 13:38:38 -0400

    add 1) Constraint Customize Lie Group-Optimal Shape 2) add some warnings 3) change status of fmincon

--- extrinsic_utils/optimization/optimizeConstraintCustomizedLieGroupCost.m
+++ extrinsic_utils/optimization/optimizeConstraintCustomizedLieGroupCost.m
@@ -35,50 +35,90 @@ function opt = optimizeConstraintCustomizedLieGroupCost(opt, X, target_size, box
 
 %     computeConstraintCustomizedLieGroupCost(X, zeros(1,3), zeros(1,3), 1, 0.2)
     prob = optimproblem;
-    f = fcn2optimexpr(@computeConstraintCustomizedLieGroupCost, X, ...
-                       R_v, T_v, target_size, box_width);
+    
+    if string(opt.method) == "Constraint Customize Lie Group"
+        f = fcn2optimexpr(@computeConstraintCustomizedLieGroupCost, X, ...
+                           R_v, T_v, target_size, box_width);
+    elseif string(opt.method) == "Constraint Customize Lie Group-Optimal Shape"
+        [~, X_h] = checkHomogeneousCoord(X(1:3, :), 1);
+        [sorted_vertices, ~, ~] = sortVertices([], opt.ideal_frame);
+        num_edges = size(sorted_vertices, 2);
+        edge_lines = zeros(num_edges, 3);
+        
+        for edge_i = 1:num_edges
+            edge_j = max(mod(edge_i+1, num_edges+1), 1);
+            v1 = sorted_vertices(2:3, edge_i);
+            v2 = sorted_vertices(2:3, edge_j);
+            
+            if abs(v1(1)- v2(1)) < 1e-5
+                % x = v1(1) -> x - v1(1) = 0
+                edge_lines(edge_i, :) = [1, 0, -v1(1)];
+            else
+                p = polyfit([v1(1) v2(1)], [v1(2), v2(2)], 1); 
+                % y = p(1)x + p(2) -> p(1)x - y + p(2)
+                edge_lines(edge_i, :) = [p(1), -1, p(2)];
+            end
+        end
+        
+        x_std = std(sorted_vertices, 0, 2);
+        if x_std>1e-5
+            warning("The ideal frame is not perpendicular to x-axis, the optimization might fail")
+        end
+        X_trans = opt.H_init*X_h;
+        h1 = scatter3(opt.figure(1).axes, X_trans(1,:), X_trans(2, :), X_trans(3,:), 'b.');
+        viewCurrentPlot(opt.figure(1).axes, "Optimization-step0", [-89, 0])
+%         computeConstraintCustomizedLieGroupOptimalShapeCost(sorted_vertices, edge_lines, X_h, ones(1, 3), zeros(1, 3), box_width);
+        f = fcn2optimexpr(@computeConstraintCustomizedLieGroupOptimalShapeCost, ...
+                                   sorted_vertices, edge_lines, X_h, R_v, T_v, box_width);        
+    else
+        error('no such optimization method: %s', opt.method)
+    end
     prob.Objective = f;
 
 	if isstruct(opt)
-        R = rotx(opt.rpy_init(1)) * roty(opt.rpy_init(2)) * rotz(opt.rpy_init(3));
-        init_Rv = Log_SO3(R);
+        init_Rv = Log_SO3(opt.H_init(1:3, 1:3));
         x0.R_v = init_Rv;
-        x0.T_v = opt.T_init;
-	else
-        R = rotx(opt(1)) * roty(opt(2)) * rotz(opt(3));
-        init_Rv = Log_SO3(R);
+        x0.T_v = opt.H_init(1:3, 4);
+    else
+        init_Rv = Log_SO3(opt.H_init(1:3, 1:3));
         x0.R_v = init_Rv;
         if length(opt) > 3
             x0.T_v = opt(4:6);
         else
             x0.T_v = [0 0 0];
         end
-
 	end
-
+%     opt.H_init
     % x0.theta_x = opt.rpy_init(1);
     % x0.theta_y = opt.rpy_init(2);
     % x0.theta_z = opt.rpy_init(3);
     % x0.T = opt.H_init(1:3, 4);
 %             options = optimoptions('fmincon', 'MaxIter',5e2,'Display','iter', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxFunctionEvaluations', 3e4);
-    options = optimoptions('fminunc', 'MaxIter',5e2, 'Display','off', 'TolX', 1e-6, 'TolFun', 1e-6);
+%     options = optimoptions('fminunc', 'MaxIter',5e3, 'Display','iter', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxFunctionEvaluations', 3e4);
+    options = optimoptions('fminunc', 'MaxIter',5e3, 'Display','iter', 'TolX', 1e-6, ...
+                'TolFun', 1e-6, 'MaxFunctionEvaluations', 3e4, 'Algorithm','quasi-newton');
+
+%     [sol, fval, status, ~] = solve(prob, x0, 'Options', options);
     max_trail = 5;
     num_tried = 1;
-    status = 0;
-    while status <=0 
+    status = -1;
+    while status < 0 
+%         [sol,fval,status,osur] = surrogateopt(f,lb,ub,opts);
         [sol, fval, status, ~] = solve(prob, x0, 'Options', options);
-        if status <=0 
+        if status < 0 
             warning("optimization failed")
         end
         num_tried = num_tried + 1;
         if (num_tried + 1 > max_trail)
-            warning("tried too many time, optimization still failed, current status:")
-            disp(status)
-            break;
+            error("tried too many time, optimization still failed, current status: %s", status)
         end
     end
     opt.H_opt = eye(4);
     opt.H_opt(1:3, 1:3) = Exp_SO3(sol.R_v);
     opt.H_opt(1:3, 4) = sol.T_v';
     opt.opt_total_cost = fval;
+    
+%     X_final = opt.H_opt*X_h;
+%     h2 = scatter3(opt.figure(1).axes, X_final(1,:), X_final(2, :), X_final(3,:), 'r*');
+%     viewCurrentPlot(opt.figure(1).axes, "Optimization-step2", [-89, 0])
 end
