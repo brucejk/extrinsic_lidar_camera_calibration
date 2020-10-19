4518789be7bea4572bb609913afcccdc8f70a1b8
Brucebot <bjhuang@umich.edu>
2020-10-19 13:08:12 -0400

    1) add lb,ub for translation

--- extrinsic_utils/optimization/optimize4PointsEulerAngle.m
+++ extrinsic_utils/optimization/optimize4PointsEulerAngle.m
@@ -42,6 +42,13 @@ function [H_LC, P, total_cost, RMSE] = optimize4PointsEulerAngle(opt, X, Y, intr
         theta_x = optimvar('theta_x', 1, 1,'LowerBound',-180,'UpperBound',180); % 1x1
         theta_y = optimvar('theta_y', 1, 1,'LowerBound',-180,'UpperBound',180); % 1x1
         theta_z = optimvar('theta_z', 1, 1,'LowerBound',-180,'UpperBound',180); % 1x1
+        
+        if isfield(opt, 'T_lb') && isfield(opt, 'T_ub')
+            T_v = optimvar('T_v', 1, 3, 'LowerBound', opt.T_lb, 'UpperBound', opt.T_ub);
+        else
+        warning("Using lb: -0.5, ub: 0.5 for translation")
+        T_v = optimvar('T_v', 1, 3, 'LowerBound', -0.5, 'UpperBound', 0.5);
+    end
         T = optimvar('T', 1, 3,'LowerBound',-0.5,'UpperBound',0.5);
         prob = optimproblem;
         
@@ -50,9 +57,11 @@ function [H_LC, P, total_cost, RMSE] = optimize4PointsEulerAngle(opt, X, Y, intr
 
         if isstruct(opt)
             x0.theta_x = opt.rpy_init(1);
-            x0.theta_y = opt.rpy_init(2) ;
+            x0.theta_y = opt.rpy_init(2);
             x0.theta_z = opt.rpy_init(3);
-            if isfield(opt, 'init_T')
+            if isfield(opt, 'T_init')
+                x0.T = opt.T_init;
+            elseif isfield(opt, 'init_T')
                 x0.T = opt.init_T;
             else
                 x0.T = [0 0 0];
@@ -67,7 +76,7 @@ function [H_LC, P, total_cost, RMSE] = optimize4PointsEulerAngle(opt, X, Y, intr
                 x0.T = [0 0 0];
             end
         end
-
+        cost4Points(x0.theta_x, x0.theta_y, x0.theta_z, x0.T, X, Y, intrinsic)
         
         options = optimoptions('fmincon', 'MaxIter',5e2, 'TolX', 1e-12, 'Display','off', 'FunctionTolerance', 1e-8, 'MaxFunctionEvaluations', 3e4);
         [sol, fval, ~, ~] = solve(prob, x0, 'Options', options);
